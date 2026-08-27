c54b9f721b1ba76e43f75429968f0bab3aa81d7e
m9511003 <m9511003@gmail.com>
2026-08-27 13:38:47 +0900

    add pandas

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-print('hello12')
-print('SDJ')
+import numpy as np
+import pandas as pd
+
 
 
 def test(a, b):
@@ -8,3 +9,8 @@ def test(a, b):
 
 result = test(5, 3)
 print(result)
+
+arr = np.array([1, 2, 3, 4, 5])
+df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+
+print(df)
